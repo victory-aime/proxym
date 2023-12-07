8c79a6a4649cfeade3ea88dd562c8d9efc8f71da
Victory MBENZE <victory.mbenze@proxym-it.com>
2023-12-07 23:06:49 +0100

    login form

---
+++ src/validators/LoginSchema.tsx
@@ -0,0 +1,17 @@
+import * as Yup from "yup";
+
+const LoginSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/,
+      "Password must contain at least one uppercase letter, one lowercase letter, and one number"
+    )
+    .required("Required"),
+});
+
+export default LoginSchema;
